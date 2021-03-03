f70ce550f9c73de8464e52224020d4033d1e85f6
Daxa Bhalani <daxabhalani001@gmail.com>
2021-03-03 21:12:50 +0530

    gih pages deployment

--- src/modules/common/CategoryLabel.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import '../../assets/css/home.css'
-
-export default function CategoryLabel({ category, color }) {
-    return (
-        <span className="social-jokes" style={{backgroundColor:color}}>{category}</span>
-    )
-}
